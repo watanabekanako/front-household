623dfee365b67cfe384f77385e81e97fb986fbc7
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-03-02 17:37:15 +0900

    スタイル実装途中

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import DefaultLayout from '../components/layout/dafaultLayout'
 import PrimaryButton from '../components/button/PrimaryButton'
+import LoginStyle from '../styles/pages/login.module.scss'
 const Login = () => {
   return (
   <>
    <DefaultLayout>
     <PrimaryButton children={"ログインする"} onClick={()=>alert("登録できました")}/>
-    </DefaultLayout></>
+    <p>アカウントを持っていませんか？</p>
+    <p className={LoginStyle.txtOrange}>ログインする</p>
+    </DefaultLayout>
+    </>
   )
 }
 
