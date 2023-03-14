4697c92b7c0caa102f8d6e505b40a6ec0a50e588
kayahara mai <mai.kayahara@rakus-partners.co.jp>
2023-03-14 16:02:46 +0900

    レポートフォームに初期値設定途中

--- src/components/form/reportForm.tsx
+++ src/components/form/reportForm.tsx
@@ -6,19 +6,22 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 
 const ReportForm = () => {
-  const [memo, setMemo] = useState("");
+  const [onePost, setOnePost] = useState<any>([]);
+  console.log(onePost.id, 90);
+  const [memo, setMemo] = useState(onePost.content);
   const [price, setPrice] = useState(0);
-  const [onePost, setOnePost] = useState([]);
+
   const dispatch = useDispatch();
   const params = useParams();
+  console.log(onePost.content, 11);
 
   useEffect(() => {
-    const fetchOnePost = async () => {
-      const response = await axios.get(`/post/${params.id}`);
-      setOnePost(response.data);
-    };
-    fetchOnePost();
+    axios.get(`/post/${params.id}`).then((res) => setOnePost(res.data));
   }, []);
+
+  // console.log(onePost.content);
+
+  const postDate = onePost.createdAt?.slice(0, 10);
 
   const changeMemo = (e: any) => {
     setMemo(e.target.value);
@@ -33,34 +36,37 @@ const ReportForm = () => {
   return (
     <div className={reportPostStyle.container}>
       {/* formで日付、メモ、金額をまとめてdispatchするか */}
-      {onePost.map((post: any) => {
-        return (
-          <form>
-            <div className={reportPostStyle.postList}>
-              <label htmlFor="date">日付</label>
-              <input
-                type="date"
-                id="date"
-                onChange={(e: any) => dispatch(inputDate(e.target.value))}
-              />
-            </div>
-            <div className={reportPostStyle.postList}>
-              <label htmlFor="memo">メモ</label>
-              <input type="text" id="memo" value={memo} onChange={changeMemo} />
-            </div>
-            <div className={reportPostStyle.postList}>
-              <label htmlFor="expence">支出</label>
-              <input
-                type="text"
-                id="expence"
-                value={post.price}
-                onChange={handleExpence}
-              />
-              円
-            </div>
-          </form>
-        );
-      })}
+      <form>
+        <div className={reportPostStyle.postList}>
+          <label htmlFor="date">日付</label>
+          <input
+            type="date"
+            id="date"
+            value={postDate}
+            onChange={(e: any) => dispatch(inputDate(e.target.value))}
+          />
+        </div>
+        <div className={reportPostStyle.postList}>
+          <label htmlFor="memo">メモ</label>
+          <input
+            type="text"
+            id="memo"
+            value={memo}
+            // placeholder={onePost.content}
+            onChange={changeMemo}
+          />
+        </div>
+        <div className={reportPostStyle.postList}>
+          <label htmlFor="expence">支出</label>
+          <input
+            type="text"
+            id="expence"
+            placeholder={onePost.price}
+            onChange={handleExpence}
+          />
+          円
+        </div>
+      </form>
     </div>
   );
 };
